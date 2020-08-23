bc1018bd41430c4a32c0c098f300b7d4584e01a4
jeremie-gauthier <jeremiegthr@gmail.com>
2020-08-23 12:11:36 +0200

    close file after write

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -5,8 +5,8 @@
   Simple comment
 */
 
-#define S "#include<stdio.h>%c#include<unistd.h>%c%c/*%c  Simple comment%c*/%c%c#define S %c%s%c%c#define PRINT(f) fprintf(f,S,10,10,10,10,10,10,10,34,S,34,10,10,34,34,34,34,10,10)%c#define FT int main(){FILE *f=fopen(%cGrace_kid.c%c,%cw%c);PRINT(f);}%c%cFT"
+#define S "#include<stdio.h>%c#include<unistd.h>%c%c/*%c  Simple comment%c*/%c%c#define S %c%s%c%c#define PRINT(f) fprintf(f,S,10,10,10,10,10,10,10,34,S,34,10,10,34,34,34,34,10,10)%c#define FT int main(){FILE *f=fopen(%cGrace_kid.c%c,%cw%c);PRINT(f);fclose(f);}%c%cFT"
 #define PRINT(f) fprintf(f,S,10,10,10,10,10,10,10,34,S,34,10,10,34,34,34,34,10,10)
-#define FT int main(){FILE *f=fopen("Grace_kid.c","w");PRINT(f);}
+#define FT int main(){FILE *f=fopen("Grace_kid.c","w");PRINT(f);fclose(f);}
 
 FT
